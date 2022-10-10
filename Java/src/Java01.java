4c69687838459f8821f9dbdf24d2864e5ff14695
EnesAyhan <enesayhan3232@gmail.com>
2022-10-11 00:04:31 +0300

    02 ve 03 eklendi

---
+++ Java/src/Java01.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Java01 {
+
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int s=scan.nextInt();
+        int a = scan.nextInt();
+        int i=scan.nextInt();
+        // Complete this line
+        // Complete this line
+
+        System.out.println(s);
+        System.out.println(a);
+        System.out.println(i);
+        // Complete this line
+        // Complete this line
+    }
+
+
+
+}
--- Java/src/java01_java.java
+++
@@ -1,5 +0,0 @@
-public class java01_javaregex {
-    public static void main(String[] args) {
-        System.out.println("first commit");
-    }
-}
